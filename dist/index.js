9b1c41166acd5b1b707001e3bb72550e2b066806
Bryan MacFarlane <bryanmacfarlane@github.com>
2020-03-26 10:44:11 -0400

    fix version dir

--- dist/index.js
+++ dist/index.js
@@ -4601,7 +4601,7 @@ function downloadGo(versionSpec, stable) {
                 core_1.debug(`extracted to ${extPath}`);
                 // extracts with a root folder that matches the fileName downloaded
                 const toolRoot = path.join(extPath, 'go');
-                toolPath = yield tc.cacheDir(toolRoot, 'go', match.version);
+                toolPath = yield tc.cacheDir(toolRoot, 'go', makeSemver(match.version));
             }
         }
         catch (error) {
--- src/installer.ts
+++ src/installer.ts
@@ -33,7 +33,7 @@ export async function downloadGo(
 
       // extracts with a root folder that matches the fileName downloaded
       const toolRoot = path.join(extPath, 'go');
-      toolPath = await tc.cacheDir(toolRoot, 'go', match.version);
+      toolPath = await tc.cacheDir(toolRoot, 'go', makeSemver(match.version));
     }
   } catch (error) {
     throw new Error(`Failed to download version ${versionSpec}: ${error}`);
