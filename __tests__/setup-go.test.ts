37335c7bb261b353407cff977110895fa0b4f7d8
Thomas Boop <52323235+thboop@users.noreply.github.com>
2020-10-01 10:45:00 -0400

    Swap to Environment Files (#76)
    * Swap to env files

--- __tests__/setup-go.test.ts
+++ __tests__/setup-go.test.ts
@@ -35,6 +35,11 @@ describe('setup-go', () => {
   let execSpy: jest.SpyInstance;
   let getManifestSpy: jest.SpyInstance;
 
+  beforeAll(() => {
+    process.env['GITHUB_PATH'] = ''; // Stub out ENV file functionality so we can verify it writes to standard out
+    console.log('::stop-commands::stoptoken'); // Disable executing of runner commands when running tests in actions
+  });
+
   beforeEach(() => {
     // @actions/core
     inputs = {};
@@ -90,7 +95,9 @@ describe('setup-go', () => {
     //jest.restoreAllMocks();
   });
 
-  afterAll(async () => {}, 100000);
+  afterAll(async () => {
+    console.log('::stoptoken::'); // Re-enable executing of runner commands when running tests in actions
+  }, 100000);
 
   it('can find 1.9.7 from manifest on osx', async () => {
     os.platform = 'darwin';
