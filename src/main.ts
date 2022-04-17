ad70bef2ef3d4a69511a3994c9b65a3584bbe653
Vladimir Safonkin <vsafonkin@github.com>
2022-04-17 17:36:51 +0200

    Fix condition for old go versions

--- src/main.ts
+++ src/main.ts
@@ -69,7 +69,7 @@ export async function addBinToPath(): Promise<boolean> {
   }
 
   let buf = cp.execSync('go env GOPATH');
-  if (buf) {
+  if (buf.length > 1) {
     let gp = buf.toString().trim();
     core.debug(`go env GOPATH :${gp}:`);
     if (!fs.existsSync(gp)) {
