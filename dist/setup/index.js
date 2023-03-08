c7f2c70396d2dc5d3176b548ab5bb4127d0e594c
Dmitry Shibanov <shibanov-1997@inbox.ru>
2023-03-08 13:31:51 +0100

    add warning if go-version is empty

--- dist/setup/index.js
+++ dist/setup/index.js
@@ -63600,6 +63600,9 @@ function run() {
                 core.debug(`add bin ${added}`);
                 core.info(`Successfully set up Go version ${versionSpec}`);
             }
+            else {
+                core.info('[warning]go-version input was not specified. The action will try to use pre-installed version.');
+            }
             const goPath = yield io.which('go');
             const goVersion = (child_process_1.default.execSync(`${goPath} version`) || '').toString();
             if (cache && cache_utils_1.isCacheFeatureAvailable()) {
